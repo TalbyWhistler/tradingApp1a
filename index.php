c6fd777495df0007c862ad9e4c69fb74535b67e8
TalbyWhistler <000748591@mohawkcollege.ca>
2026-06-04 17:01:43 -0400

    up to date the sequal?

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trading App Temporary Front Page</title>
-   <!-- <link rel='stylesheet' href='css/tradingStyles.css'> --> 
+   <!-- <link rel='stylesheet' href='css/tradingStyles.css'> -->
 </head>
 <body>
     <?php include 'php/trading_frontend_0.php' ?>
